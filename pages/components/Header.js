fd7e0b9ce9fbed571fa3006e6e31c9e09d02e928
WildHashGhost_Cica <wildhashghost@gmail.com>
2021-07-20 14:18:02 +0200

    div -> header

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -3,7 +3,7 @@ import Icon from "@material-tailwind/react/Icon";
 
 function Header() {
     return (
-        <div className='sticky top-0 z-50 flex items-center px-4 py-2 
+        <header className='sticky top-0 z-50 flex items-center px-4 py-2 
         shadow-md-yellow bg-white'>
               <Button
             color="gray"
@@ -43,7 +43,7 @@ function Header() {
         alt=""
         />
 
-        </div>
+        </header>
     );
 }
 
