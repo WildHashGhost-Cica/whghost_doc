0bba6c767fcc6740ae6538eb82e6a05faada2054
WildHashGhost_Cica <wildhashghost@gmail.com>
2021-07-20 17:18:07 +0200

    new document section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Head from 'next/head'
 import Header from './components/Header'
+import Button from "@material-tailwind/react/Button";
+import Icon from "@material-tailwind/react/Icon";
+import Image from "next/image"
 
 export default function Home() {
   return (
@@ -9,6 +12,32 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header/>
+      <section className="bg-[#F8F9FA] pb-10 px-10">
+        <div className="max-w-3xl mx-auto">
+          <div className="flex items-center justify-between py-6">
+            <h2 className='text-gray-700 text-lg'>Start a new document</h2>
+            <Button
+            color="gray"
+            buttonType="outline"
+            iconONly={true}
+            ripple="dark"
+            className="border-0"
+            >
+              <Icon name="more_vert" size="3xl"/>
+
+            </Button>
+          </div>
+          <div>
+            <div>
+              <div className="relative h-52 w-40 border-2 cursor-pointer hover:border-blue-700">
+                <Image src="https://links.papareact.com/pju"
+                layout='fill'/>
+              </div>
+            </div>
+            <p className="ml-2 mt-2 font-semibold text-sm text-gray-700">Blank</p>
+          </div>
+        </div> 
+      </section>
 
       
     </div>
