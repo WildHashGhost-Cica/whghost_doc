6bac460a25aa7b9ed5026ea3e99a600a541d408d
WildHashGhost_Cica <wildhashghost@gmail.com>
2021-07-21 19:09:45 +0200

    add create new document modal

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,58 @@ import Icon from "@material-tailwind/react/Icon";
 import Image from "next/image";
 import  { getSession, useSession } from "next-auth/client";
 import Login from './components/Login';
+import { useState } from 'react';
+import ModalBody from "@material-tailwind/react/ModalBody";
+import ModalFooter from "@material-tailwind/react/ModalFooter";
+import Modal from "@material-tailwind/react/Modal"
 
 export default function Home() {
 
   const [session] = useSession();
+  const [showModal, setShowModal] = useState(false);
+  const [input, setInput] = useState("");
 
-  if(!session) return <Login/>
+  if(!session) return <Login/>;
+
+  const createDocument = () => {};
+
+  const modal = (
+    <Modal
+    size="sm"
+    active={showModal}
+    toggler={() => setShowModal(false)}
+    >
+
+    <ModalBody>
+      <input
+      value={input}
+      onChange={(e) => setInput(e.target.value)}
+      type="text"
+      className="outline-none w-full"
+      placeholder="Enter name of document..."
+      onKeyDown={(e) => e.key === "Enter" && createDocument()}
+      />
+    </ModalBody>
+    <ModalFooter>
+      <Button 
+      color="blue"
+      buttonType="link"
+      onClick={(e) => setShowModal(false)}
+      ripple="dark"
+      >
+        Cancel
+      </Button>
+      <Button
+      color="blue"
+      onClick={createDocument}
+      ripple="light"
+      >
+        Create
+      </Button>
+    </ModalFooter>
+
+    </Modal>
+  );
 
   return (
     <div>
@@ -19,6 +65,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header/>
+      {modal}
+      
+
       <section className="bg-[#F8F9FA] pb-10 px-10">
         <div className="max-w-3xl mx-auto">
           <div className="flex items-center justify-between py-6">
@@ -36,7 +85,7 @@ export default function Home() {
           </div>
           <div>
             <div>
-              <div className="relative h-52 w-40 border-2 cursor-pointer hover:border-blue-700">
+              <div onClick={() => setShowModal(true)} className="relative h-52 w-40 border-2 cursor-pointer hover:border-blue-700">
                 <Image src="https://links.papareact.com/pju"
                 layout='fill'/>
               </div>
